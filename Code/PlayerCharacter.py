e731777424a62bbd4ca5e7bc238c86387ba2c208
Damon Stamper <damon.stamper@gmail.com>
2020-04-01 12:31:19 -0400

    Feature 3 Alignment: adding PlayerCharacter alignment

--- Code/PlayerCharacter.py
+++ Code/PlayerCharacter.py
@@ -3,8 +3,9 @@ import pytest
 class PlayerCharacter:
     """The entity that is played"""
 
-    def __init__(self, name):
+    def __init__(self, name, alignment='Neutral'):
         self.name = name
+        self.alignment = alignment
     
     @property
     def name(self):
@@ -16,6 +17,19 @@ class PlayerCharacter:
             self.__name = new_name
         else: 
             raise Exception('Please provide a valid name (string).')
+
+    @property
+    def alignment(self):
+        return self.__alignment
+
+    @alignment.setter
+    def alignment(self, new_alignment):
+        alignments = ['Good', 'Evil', 'Neutral']
+        if (new_alignment in alignments):
+            self.__alignment = new_alignment
+        else: 
+            raise Exception(f"Please provide a valid alignment. Valid alignments are {alignments}.")
+
 
 def test_PlayerCharacter_CreateCorrectName():
     PlayerCharacter('Foo')
@@ -37,3 +51,16 @@ def test_PlayerCharacter_RenameIncorrectName():
     player = PlayerCharacter('Foo')
     with pytest.raises(Exception):
         player.name = 7
+
+def test_PlayerCharacter_CreateDefaultAlignment():
+    player = PlayerCharacter('Foo')
+    assert player.alignment == 'Neutral'
+
+
+def test_PlayerCharacter_CreateSpecificAlignment():
+    player = PlayerCharacter('Foo', 'Good')
+    assert player.alignment == 'Good'
+
+def test_PlayerCharacter_CreateIncorrectAlignment():
+    with pytest.raises(Exception):
+        PlayerCharacter('Foo', 'Something')
