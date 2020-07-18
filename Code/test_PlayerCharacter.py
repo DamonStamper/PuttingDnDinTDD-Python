e0edd89c88b9a8bcefd3a9c01a3fb5a5255e09cf
Damon Stamper <damon.stamper@gmail.com>
2020-07-17 23:23:30 -0400

    Refactor with suggestions from SonarCloud

--- Code/test_PlayerCharacter.py
+++ Code/test_PlayerCharacter.py
@@ -15,49 +15,49 @@ def test_playercharacter_armorclass_create_incorrect_name():
         PlayerCharacter.PlayerCharacter(7)
 
 def test_playercharacter_get_correct_name():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
-    assert Player.name == 'Foo'
+    player = PlayerCharacter.PlayerCharacter('Foo')
+    assert player.name == 'Foo'
 
 def test_playercharacter_rename_correct_name():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
-    Player.name = 'Bar'
-    assert Player.name == 'Bar'
+    player = PlayerCharacter.PlayerCharacter('Foo')
+    player.name = 'Bar'
+    assert player.name == 'Bar'
 
 def test_playercharacter_rename_incorrect_name():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
+    player = PlayerCharacter.PlayerCharacter('Foo')
     with pytest.raises(Exception):
-        Player.name = 7
+        player.name = 7
 
 def test_playercharacter_create_default_alignment():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
-    assert Player.alignment == 'Neutral'
+    player = PlayerCharacter.PlayerCharacter('Foo')
+    assert player.alignment == 'Neutral'
 
 
 def test_playercharacter_create_specific_alignment():
-    Player = PlayerCharacter.PlayerCharacter('Foo', alignment='Good')
-    assert Player.alignment == 'Good'
+    player = PlayerCharacter.PlayerCharacter('Foo', alignment='Good')
+    assert player.alignment == 'Good'
 
 def test_playercharacter_create_incorrect_alignment():
     with pytest.raises(Exception):
         PlayerCharacter.PlayerCharacter('Foo', alignment='Something')
 
 def test_playercharacter_create_default_armorclass():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
-    assert Player.armorclass == 10
+    player = PlayerCharacter.PlayerCharacter('Foo')
+    assert player.armorclass == 10
 
 def test_playercharacter_create_specific_armorclass():
-    Player = PlayerCharacter.PlayerCharacter('Foo', armorclass=7)
-    assert Player.armorclass == 7
+    player = PlayerCharacter.PlayerCharacter('Foo', armorclass=7)
+    assert player.armorclass == 7
 
 def test_playercharacter_create_default_hitpoints():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
-    assert Player.hitpoints == 5
+    player = PlayerCharacter.PlayerCharacter('Foo')
+    assert player.hitpoints == 5
 
 def test_playercharacter_create_specifict_hitpoints():
-    Player = PlayerCharacter.PlayerCharacter('Foo', hitpoints=7)
-    assert Player.hitpoints == 7
+    player = PlayerCharacter.PlayerCharacter('Foo', hitpoints=7)
+    assert player.hitpoints == 7
 
 def test_playercharacter_can_attack():
-    Player = PlayerCharacter.PlayerCharacter('Foo')
+    player = PlayerCharacter.PlayerCharacter('Foo')
     Enemy = PlayerCharacter.PlayerCharacter('Bar')
-    Player.attack(Enemy)
+    player.attack(Enemy)
