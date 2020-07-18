07e3860c7a458fec95f051319448897098b7df02
Damon Stamper <damon.stamper@gmail.com>
2020-07-17 21:03:17 -0400

    Refactor with suggestions from SonarCloud

--- Code/test_PlayerCharacter.py
+++ Code/test_PlayerCharacter.py
@@ -3,61 +3,61 @@ import unittest.mock
 
 import PlayerCharacter
 
-def test_PlayerCharacter_IncorrectNoParameters():
+def test_playercharacter_incorrect_no_parameters():
     with pytest.raises(Exception):
         PlayerCharacter.PlayerCharacter()
 
-def test_PlayerCharacterArmorClass_CreateCorrectName():
+def test_playercharacter_armorclass_create_correct_name():
     PlayerCharacter.PlayerCharacter('Foo')
 
-def test_PlayerCharacterArmorClasster_CreateIncorrectName():
+def test_playercharacter_armorclass_create_incorrect_name():
     with pytest.raises(Exception):
         PlayerCharacter.PlayerCharacter(7)
 
-def test_PlayerCharacter_GetCorrectName():
+def test_playercharacter_get_correct_name():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     assert Player.name == 'Foo'
 
-def test_PlayerCharacter_RenameCorrectName():
+def test_playercharacter_rename_correct_name():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     Player.name = 'Bar'
     assert Player.name == 'Bar'
 
-def test_PlayerCharacter_RenameIncorrectName():
+def test_playercharacter_rename_incorrect_name():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     with pytest.raises(Exception):
         Player.name = 7
 
-def test_PlayerCharacter_CreateDefaultAlignment():
+def test_playercharacter_create_default_alignment():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     assert Player.alignment == 'Neutral'
 
 
-def test_PlayerCharacter_CreateSpecificAlignment():
+def test_playercharacter_create_specific_alignment():
     Player = PlayerCharacter.PlayerCharacter('Foo', alignment='Good')
     assert Player.alignment == 'Good'
 
-def test_PlayerCharacter_CreateIncorrectAlignment():
+def test_playercharacter_create_incorrect_alignment():
     with pytest.raises(Exception):
         PlayerCharacter.PlayerCharacter('Foo', alignment='Something')
 
-def test_PlayerCharacter_CreateDefaultArmorClass():
+def test_playercharacter_create_default_armorclass():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     assert Player.armorclass == 10
 
-def test_PlayerCharacter_CreateSpecifictArmorClass():
+def test_playercharacter_create_specific_armorclass():
     Player = PlayerCharacter.PlayerCharacter('Foo', armorclass=7)
     assert Player.armorclass == 7
 
-def test_PlayerCharacter_CreateDefaultHitPoints():
+def test_playercharacter_create_default_hitpoints():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     assert Player.hitpoints == 5
 
-def test_PlayerCharacter_CreateSpecifictHitPoints():
+def test_playercharacter_create_specifict_hitpoints():
     Player = PlayerCharacter.PlayerCharacter('Foo', hitpoints=7)
     assert Player.hitpoints == 7
 
-def test_PlayerCharacter_CanAttack():
+def test_playercharacter_can_attack():
     Player = PlayerCharacter.PlayerCharacter('Foo')
     Enemy = PlayerCharacter.PlayerCharacter('Bar')
     Player.attack(Enemy)
